7640e184405743b0832b71c5a60ecd7e9cf32566
Morne Maritz <Morne.Maritz@allangray.co.za>
2017-01-06 17:00:08 +0200

    up to lesson 16

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,24 @@
+export function increment(index){
+    return {
+       type: 'INCREMENT_LIKES',
+       index 
+    }
+}
+
+export function addComment(postId, author, comment) {
+    console.log("dispatching addComment")
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+export function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        postId,
+        i
+    }
+}
---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,31 @@
+function postComments(state = [], action){
+    switch(action.type){
+        case "ADD_COMMENT":
+            console.log("adding the comment");
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }]
+        case "REMOVE_COMMENT":
+            console.log("removing a the comment");
+            return [
+                ...state.slice(0, action.i),
+                ...state.slice(action.i + 1)
+            ]
+        default:
+            return state;
+    }
+}
+
+function comments(state = [], action) {
+    if(typeof action.postId !== 'undefined'){
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
+
+    return state;
+}
+
+export default comments
